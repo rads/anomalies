2f2a79eb78ea4da6ec94dbe8cb8048cc92c32d41
Radford Smith <contact@radsmith.com>
2017-12-22 07:42:18 -0800

    Add CLJS support

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject rads/anomalies "0.1.13"
+  :description "Categoric partitioning of program failures.
+               (This is a fork of com.cognitect/anomalies to provide CLJS support.)"
+  :url "https://github.com/rads/anomalies"
+  :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
+                 [org.clojure/clojurescript "1.9.946" :scope "provided"]])
